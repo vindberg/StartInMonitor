04a11d2d9de8dc483eb901006bb11d5b4e978ceb
vindberg <av@validators.com>
2021-09-10 14:06:35 +0200

    Add project files.

---
+++ StartInMonitor/Model.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartInMonitor
+{
+    public class Model
+    {
+        /// <summary>
+        /// The monitor number (starts with 1 which is the main monitor)
+        /// </summary>
+        public int Monitor { get; set; } = 1;
+
+        /// <summary>
+        /// Full path and name of the application
+        /// </summary>
+        public string Application { get; set; }
+
+        /// <summary>
+        /// Argumments for the application
+        /// </summary>
+        public string Arguments { get; set; }
+
+        /// <summary>
+        /// Pixels away from the left side of the monitor/screen
+        /// </summary>
+        public int Left { get; set; }
+        
+        /// <summary>
+        /// Pixels away from the top of the monitor/screen
+        /// </summary>
+        public int Top { get; set; }
+
+        /// <summary>
+        /// Pixels that are subtracted from the total width of the monitor/screen. The application is defaulted to full width.
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// Pixels that are subtracted from the total heigth of the monitor/screen. The application is defaulted to full height.
+        /// </summary>
+        public int Height { get; set; }
+
+    }
+}
---
+++ StartInMonitor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartInMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = new Model()
+            {
+                Monitor = int.Parse(args[0]),
+                Application = args[1],
+                Arguments = args[2],
+                Left = int.Parse(args[3]),
+                Top = int.Parse(args[4]),
+                Width = int.Parse(args[5]),
+                Height = int.Parse(args[6]),
+            };
+
+            Start(model);
+        }
+
+        public static void Start(Model model)
+        {
+            var allScreens = Screen.AllScreens.ToList();
+
+            var screenOfChoice = allScreens.First();
+            if (allScreens.Count >= model.Monitor)
+            {
+                screenOfChoice = allScreens[(model.Monitor - 1)];
+            }
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    Arguments = model.Arguments,
+                    FileName = model.Application,
+                    WindowStyle = ProcessWindowStyle.Normal
+                }
+            };
+
+            process.Start();
+
+            Thread.Sleep(1000);
+
+            MoveWindow(process.MainWindowHandle, (screenOfChoice.WorkingArea.Left + model.Left), (screenOfChoice.WorkingArea.Top + model.Top), (screenOfChoice.WorkingArea.Width - model.Width), (screenOfChoice.WorkingArea.Height - model.Height), false);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+    }
+}
